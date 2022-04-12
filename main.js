fc71497a63ed547972408add96c9700690728b44
kashanmahmood <kashan.mahmood.24@dartmouth.edu>
2022-04-12 16:55:33 -0400

    update

---
+++ main.js
@@ -0,0 +1,18 @@
+// var numQuestions = 3;
+// var qAnswered = new Array();
+
+// $('body').on('click', function(){
+
+//     for(let qNum= 1; qNum <= numQuestions; qNum++ ){
+
+//         if($(`input[name='question_$qNum']:checked`).length != 0){
+
+//             document.getElementById(`q${qNum}`)
+
+//         }
+//     }
+    
+//     $(this).parent().find('input:not(:checked)').attr( "disabled", "disabled" );
+
+
+// })
