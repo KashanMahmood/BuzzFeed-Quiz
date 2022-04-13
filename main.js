636ed1c7bcb0a986002f3f9dfcf5d0d31b7e1cc5
kashanmahmood <kashan.mahmood.24@dartmouth.edu>
2022-04-13 18:31:46 -0400

    started on json file

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
-var total= 2
+var total= 2;
+var numQuestions = 0;
+
+$.getJSON("data.json", function(data) {
+ 
+    // Wrap each question section into a div with class "section"
+    // Then in that section create a div class "question" and give it the value of the question name
+    // Then create a div class answers:
+    //     for each answer of that question,
+    //         create a label with class "ansewr"
+    //         inside the lab addthe answer Image
+    //         create an input putton type radio
+
+
+    (data.questions).each(function(){
+        
+        const newSection = document.body.createElement("div").addClass("section");
+        const newQuestion = document.body.createElement('div').addClass('question').appendChild( $(this).question_name, $(this).question_img_url );
+        const answers = document.body.createElement("div").addClass("answers")
+
+        ($(this).answers).each(function(){
+            
+            // still need to worry about adding value
+            const answer = document.createElement("label").addClass("answer");
+            answer.appendChild( $(this).text, $(this).img_url);
+            var input = $('<input type="button" value="10" class="answerInput" />');
+            input.appendTo(answer)
+     
+        });
+        newSection.appendChild(newQuestion, answers);
+
+    });
+});
+
 
 $('body').on('click', '.answer', function() {
 
